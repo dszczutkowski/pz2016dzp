f506855f844a97e92412f46fde12882efeae17bf
dszczutkowski <dszczutkowski@outlook.com>
2016-11-07 15:10:58 +0100

    Baza danych, modele, interfejsy, pom

---
+++ Szkola/src/main/java/account/repository/RoleRepository.java
@@ -0,0 +1,7 @@
+package zespolowy.account.repository;
+
+import zespolowy.account.model.Role;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface RoleRepository extends JpaRepository<Role, Long>{
+}
