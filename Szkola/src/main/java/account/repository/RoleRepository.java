cf9dbe018998e45aea279894c3438184a4556202
lzawistowski <zaw.lukasz@gmail.com>
2016-11-28 14:52:36 +0100

    deleted not working files.

--- Szkola/src/main/java/account/repository/RoleRepository.java
+++
@@ -1,7 +0,0 @@
-package zespolowy.account.repository;
-
-import zespolowy.account.model.Role;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface RoleRepository extends JpaRepository<Role, Long>{
-}
--- Szkola/src/main/java/account/repository/UserRepository.java
+++
@@ -1,8 +0,0 @@
-package zespolowy.account.repository;
-
-import zespolowy.account.model.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepository extends JpaRepository<User, Long> {
-    User findByUsername(String username);
-}
