ff10ce6326b551d64c9b38bb83ad81149f91429b
lzawistowski <zaw.lukasz@gmail.com>
2016-11-28 14:58:44 +0100

    added working project.

---
+++ src/main/java/com/schoolmanager/view/controller/LoginPageController.java
@@ -0,0 +1,37 @@
+package com.schoolmanager.view.controller;
+
+import com.schoolmanager.util.stereotypes.Controller;
+import com.schoolmanager.view.model.LoginPageModel;
+import org.slf4j.Logger;
+
+import javax.faces.application.FacesMessage;
+import javax.faces.context.FacesContext;
+import javax.inject.Inject;
+import java.io.IOException;
+
+@Controller
+public class LoginPageController {
+
+    @Inject
+    private LoginPageModel loginPageModel;
+    private Logger logger;
+
+    public void login() {
+        String login = loginPageModel.getLogin();
+        String password = loginPageModel.getPassword();
+
+        if(false){
+            FacesMessage wrongLoginPasswordMsg = new FacesMessage(FacesMessage.SEVERITY_ERROR,"Wrong login/password","Wrong login/password");
+            FacesContext.getCurrentInstance().addMessage(null,wrongLoginPasswordMsg);
+        }
+
+        else { //
+            try {
+                FacesContext.getCurrentInstance().getExternalContext().redirect("timetable.xhtml");
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+
+    }
+}
