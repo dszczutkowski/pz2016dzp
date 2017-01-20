d260f4ce87b6456e105453ec31bc510ca1b3b69e
Redellex <masterpaw5679966@gmail.com>
2017-01-20 22:36:43 +0100

    Still not working table... Just local commit.

--- src/main/java/com/schoolmanager/view/controller/LessonPageController.java
+++ src/main/java/com/schoolmanager/view/controller/LessonPageController.java
@@ -1,25 +1,92 @@
 package com.schoolmanager.view.controller;
 
+import com.schoolmanager.dao.LessonRepository;
+import com.schoolmanager.dao.MemberRepository;
+import com.schoolmanager.entity.LessonEntity;
 import com.schoolmanager.util.stereotypes.Controller;
 import com.schoolmanager.view.model.LessonPageModel;
+import com.schoolmanager.view.model.ModelTest;
 
 
+import javax.annotation.PostConstruct;
 import javax.inject.Inject;
 import java.io.IOException;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.logging.Logger;
 
 @Controller
 public class LessonPageController {
 
     @Inject
-    private LessonPageModel LessonPageModel;
+    private LessonPageModel lessonPageModel;
 
     @Inject
     private Logger log;
 
+    @Inject
+    private LessonRepository lessonRepository;
+
+    @PostConstruct
+    public void init(){
+
+        log.info("HUEHUEUHEUHUEHUEHUEUHUEHUH \n");
+        List<LessonEntity> list = lessonRepository.findAllOrderedBySubject();
+        List<LessonEntity> poniedzialek = new ArrayList<LessonEntity>();
+        List<LessonEntity> wtorek = new ArrayList<LessonEntity>();
+        List<LessonEntity> sroda = new ArrayList<LessonEntity>();
+        List<LessonEntity> czwartek = new ArrayList<LessonEntity>();
+        List<LessonEntity> piatek = new ArrayList<LessonEntity>();
+        List<ModelTest> lista  = new ArrayList<ModelTest>();
+        for (LessonEntity lesson : list) {
+            String dzien = lesson.getDayofweek();
+            if (dzien.equals("poniedzialek")){
+                log.info("dupa");
+                poniedzialek.add(lesson);
+                ModelTest hue = new ModelTest();
+                hue.setDescription("HAHAHAHHAHADZIALAJKURWO");
+                lista.add(hue);
+            }
+            if (dzien.equals("wtorek")){
+                wtorek.add(lesson);
+            }
+            if (dzien.equals("sroda")){
+                sroda.add(lesson);
+            }
+            if (dzien.equals("czwartek")){
+                czwartek.add(lesson);
+            }
+            if (dzien.equals("piatek")){
+                piatek.add(lesson);
+
+            }
+        }
+        log.info("poniedzialek? " + poniedzialek.size());
+
+        lessonPageModel.setLessons(lista);
+        //lessonPageModel.setChosenKlasa("1a");
+    }
+    /*    for (LessonEntity lesson : list){
+            if(lesson.getDayofweek().equals("poniedzialek")) {
+                poniedzialek.add(lesson);
+            }
+            if(lesson.getDayofweek().equals("wtorek")) {
+                wtorek.add(lesson);
+            }
+            if(lesson.getDayofweek().equals("sroda")) {
+                sroda.add(lesson);
+            }
+            if(lesson.getDayofweek().equals("czwartek")) {
+                czwartek.add(lesson);
+            }
+            if(lesson.getDayofweek().equals("piatek")) {
+                piatek.add(lesson);
+                log.info("piatek: " + lesson.getDayofweek() + "      " + lesson.getSubject());
+            }
+        }*/
+
     public void CheckIt() {
         log.info("UEUEUUEUEUEUUEUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUEEEE");
     }
-
 }
--- src/main/java/com/schoolmanager/view/model/LessonPageModel.java
+++ src/main/java/com/schoolmanager/view/model/LessonPageModel.java
@@ -1,16 +1,45 @@
 package com.schoolmanager.view.model;
 
+import com.schoolmanager.entity.LessonEntity;
+
 import javax.enterprise.inject.Model;
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.ViewScoped;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 @Model
+@ManagedBean
+@ViewScoped
 public class LessonPageModel {
-    public String getKlasa() {
-        return klasa;
+    private List<ModelTest> lessons = new ArrayList<ModelTest>();
+
+    public List<ModelTest> getLessons() {
+        return lessons;
     }
 
-    public void setKlasa(String klasa) {
-        this.klasa = klasa;
+    public void setLessons(List<ModelTest> lessons) {
+        this.lessons = lessons;
     }
 
-    private String klasa;
+    public String getChosenKlasa() {
+        return chosenKlasa;
+    }
+
+    public void setChosenKlasa(String chosenKlasa) {
+        this.chosenKlasa = chosenKlasa;
+    }
+
+    public List<String> getKlasy() {
+        return Klasy;
+    }
+
+    public void setKlasy(List<String> klasy) {
+        Klasy = klasy;
+    }
+
+    private List<String> Klasy = Arrays.asList("1a", "1b", "1c");;
+
+    private String chosenKlasa;
 }
