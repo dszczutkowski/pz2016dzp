0273f915d72fc65e592cc398dfff0f4f6ac8853e
dszczutkowski <dszczutkowski@outlook.com>
2017-01-21 01:04:10 +0100

    Inbox is working now

--- src/main/java/com/schoolmanager/view/model/InboxPageModel.java
+++ src/main/java/com/schoolmanager/view/model/InboxPageModel.java
@@ -1,12 +1,18 @@
 package com.schoolmanager.view.model;
 
+import com.schoolmanager.dao.MessageRepository;
 import com.schoolmanager.entity.Member;
 import com.schoolmanager.entity.Message;
+import com.schoolmanager.service.LoginSession;
 
 import javax.annotation.ManagedBean;
 import javax.annotation.PostConstruct;
 import javax.enterprise.inject.Model;
+import javax.faces.bean.ManagedProperty;
 import javax.faces.bean.ViewScoped;
+import javax.inject.Inject;
+import javax.servlet.http.HttpSession;
+import java.io.IOException;
 import java.util.List;
 
 /**
@@ -14,7 +20,7 @@ import java.util.List;
  */
 
 @ManagedBean
-@ViewScoped
+@ViewScopedS
 @Model
 public class InboxPageModel {
 
@@ -36,6 +42,25 @@ public class InboxPageModel {
 
     public void setSelectedMessage(Message selectedMessage) {
         this.selectedMessage = selectedMessage;
+    }
+
+    @Inject
+    private MessageRepository messageRepository;
+
+    @Inject
+    private java.util.logging.Logger log;
+
+    @PostConstruct
+    public void init()
+    {
+        HttpSession session = LoginSession.getSession();
+        Member meh;
+        meh = (Member)session.getAttribute("USERNAME");
+        setMessages(messageRepository.findAllReceived(meh.getId()));
+        for(Message m : messages)
+        {
+            log.info(m.getText() + "\n");
+        }
     }
 
 }
